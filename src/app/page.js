d13b95ad43848e3eb3b6c69857423433aa3c59e4
tldasarath <tldasarath@gmail.com>
2025-11-06 18:49:57 +0530

    :Home section updated

--- src/app/page.js
+++ src/app/page.js
@@ -25,8 +25,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="min-h-screen">
-    <HeroSection/>
-    <Companyformation/>
+     <HeroSection/>
+     <Companyformation/>
     <AboutSection/>
     <ScheduleMeeting/>
     <ManagerSection/>
